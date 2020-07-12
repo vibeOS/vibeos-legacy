f6e59234310829e3764e86a43fd9257f2623046c
Nathan Mendenhall <nathanm0250@gmail.com>
2020-07-11 23:21:16 -0700

    updated ct and stats got new vars

--- scripts/ct.js
+++ scripts/ct.js
@@ -1,8 +1,33 @@
-// lines is an array of the last 10 or so lines displaying on the terminal
-// lines = ['line', 'blah']
+// CONFIDENTIALITY WARNING!
+// This code is marked as confidential in CT OpenCode.
+// Unauthorized trasnfers of this file can result in termination of
+// your CT OpenCode account, CT Web Services account and prosecution.
+// If you are not authorized per the object below, immediately open
+// a security incident and report all information.
 
-function runOnCmdLine(){
-	return 'YE'; // the commandline will output all return data so it should show:
-	// > runOnCmdLine()
-	// YE
+var nonprog_CTOpenCodeData = {
+	project: tnos,
+	file: ct,
+	lang: javascript,
+	owner: ctaetcsh,
+	authuser1: vibedivide,
+	confidential: yes
+}
+
+// NOTE (ctaetcsh): Important Information
+// The 'lines' array contains the last 10 lines of the terminal.
+// The terminal will output all return data.
+
+// Quick test function to ensure command is being received and output is being sent.
+function test() {
+	return 'Success';
+}
+
+function catto() {
+	//Work in Progress
+}
+
+// function can be called to test a certain object
+function diagnostics(thing) {
+
 }
--- scripts/stats.js
+++ scripts/stats.js
@@ -1,12 +1,17 @@
 var initStats=(()=>{
-	new cwin(moLs.length+1, 500 , 250, 'Stats', (ele)=>{ // info panel
+	new cwin(moLs.length+1, 500 , 250, 'Enviornment Information', (ele)=>{ // info panel
 		//console.log(ele);
 		
 		mctx.fillStyle='#000';
 		mctx.font = "16px Arial";
-		mctx.fillText(`URL: ${unescape(location.href)}`, ele.xpos + 10 , ele.ypos + 50 );
-		mctx.fillText(`Platform: ${navigator.platform}`, ele.xpos + 10 , ele.ypos + 75 );
-		
+		mctx.fillText(`URL: ${unescape(location.href)}`, ele.xpos + 10 , ele.ypos + 50 ); // Line 1
+		mctx.fillText(`Platform: ${navigator.platform}`, ele.xpos + 10 , ele.ypos + 75 ); // Line 2
+		mctx.fillText(`User Agent: ${navigator.userAgent}`, ele.xpos + 10 , ele.ypos + 100 ); // Line 3; (ct: DIVIDE WRAP THIS!)
+		mctx.fillText(`Screen Resolution: ${screen.width+"x"+screen.height}`, ele.xpos + 10 , ele.ypos + 125 ); // Line 4
+		mctx.fillText(`Window Resolution: ${screen.availWidth+"x"+screen.availHeight}`, ele.xpos + 10 , ele.ypos + 150 ); // Line 5
+		mctx.fillText(`Enviornment Resolution: ${msize.w+"x"+msize.h}`, ele.xpos + 10 , ele.ypos + 175 ); // line 6
+		mctx.fillText(`IP Address: Not Available`, ele.xpos + 10 , ele.ypos + 200 ); // line 7; (ct: this needs to get working, unfortunately the implementation is dogshit so)
+
 	}, (ele)=>{ // on closing
 		
 	}, msize.w/2, msize.h/2, 'img/info.png');

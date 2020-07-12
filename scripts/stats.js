7bfd73b0e305fb42fa340038d9343f785a8f8631
vibedivde <60160763+vibedivde@users.noreply.github.com>
2020-07-12 11:15:43 -0400

    sex

--- scripts/stats.js
+++ scripts/stats.js
@@ -1,20 +1,38 @@
-var initStats=(()=>{
-	new cwin(moLs.length+1, 500 , 250, 'Enviornment Information', (ele)=>{ // info panel
-		//console.log(ele);
+var initStats=(async()=>{
+	var ip=await request('https://api.ipify.org?format=json', 'GET');
+	
+	ip=await JSON.parse(ip.html).ip;
+	
+	new cwin(moLs.length+1, 500 , 250, 'Enviornment Information', async (ele)=>{ // called after rendering
+		var lines=[],
+			linesWrapped=[],
+			writeLine=0;;
+		console.log(ip);
 		
 		mctx.fillStyle='#000';
 		mctx.font = "16px Arial";
-		mctx.fillText(`URL: ${unescape(location.href)}`, ele.xpos + 10 , ele.ypos + 50 ); // Line 1
-		mctx.fillText(`Platform: ${navigator.platform}`, ele.xpos + 10 , ele.ypos + 75 ); // Line 2
-		mctx.fillText(`User Agent: ${navigator.userAgent}`, ele.xpos + 10 , ele.ypos + 100 ); // Line 3; (ct: DIVIDE WRAP THIS!)
-		mctx.fillText(`Screen Resolution: ${screen.width+"x"+screen.height}`, ele.xpos + 10 , ele.ypos + 125 ); // Line 4
-		mctx.fillText(`Window Resolution: ${screen.availWidth+"x"+screen.availHeight}`, ele.xpos + 10 , ele.ypos + 150 ); // Line 5
-		mctx.fillText(`Enviornment Resolution: ${msize.w+"x"+msize.h}`, ele.xpos + 10 , ele.ypos + 175 ); // line 6
-		mctx.fillText(`IP Address: Not Available`, ele.xpos + 10 , ele.ypos + 200 ); // line 7; (ct: this needs to get working, unfortunately the implementation is dogshit so)
+		
+		lines.push(`URL: ${unescape(location.href)}`);
+		lines.push(`Platform: ${navigator.platform}`);
+		lines.push(`User Agent: ${navigator.userAgent}`); // Line 3; (ct: DIVIDE WRAP THIS!) div: OK
+		lines.push(`Screen Resolution: ${screen.width+"x"+screen.height}`); // Line 4
+		lines.push(`Window Resolution: ${screen.availWidth+"x"+screen.availHeight}`); // Line 5
+		lines.push(`Enviornment Resolution: ${msize.w+"x"+msize.h}`); // line 6
+		lines.push(`IP Address: ${ip}`); // line 7; (ct: this needs to get working, unfortunately the implementation is dogshit so)
+
+		lines.forEach((e,i)=>{
+			wordWrap(e, ele.width / 7).split('\n').forEach((ee,ii)=>{
+				writeLine++;
+				linesWrapped.push(ee);
+			});
+		});
+		linesWrapped.forEach((e,i)=>{
+			mctx.fillText(e, ele.xpos + 10 , ele.ypos + 50 + i*20);
+		});
 
 	}, (ele)=>{ // on closing
 		
-	}, msize.w/2, msize.h/2, 'img/info.png');
+	}, msize.w/4, msize.h/2, 'img/info.png');
 });
 
 addDockIcon('img/info.png', 'System info', initStats);
